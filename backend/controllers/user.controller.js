adce88c1e38e8e0e1834b35b70b1e934cf22357c
Zakaria <zakariamesrar49@gmail.com>
2024-05-10 15:09:23 +0100

    admin login commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -29,3 +29,44 @@ export const getAdminForSidebar = async (req, res) => {
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
+export const getAllUsers = async (req, res) => {
+	try {
+		const Users = await User.find()
+		res.status(200).json(Users);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+export const updateUser = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const newData = req.body;
+
+		if (newData.password) {
+			const hashedPassword = await bcrypt.hash(newData.password, 10);
+			newData.password = hashedPassword;
+		}
+
+		const updatedUser = await UsersModel.findByIdAndUpdate(
+			id,
+			newData,
+			{ new: true }
+		);
+
+		if (!updatedUser) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+
+		res.json({ data: updatedUser });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+
+
+
+
+
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar , getAdminForSidebar } from "../controllers/user.controller.js";
+import { getUsersForSidebar , getAdminForSidebar , getAllUsers , updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
 router.get("/admin", protectRoute, getAdminForSidebar);
+router.get("/fetchUsers" , getAllUsers);
+router.put("/updateUserById" , updateUser);
 
 export default router;
