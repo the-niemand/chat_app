767dbc7ed16afbfbe9b9cfd39fde8fffb0a2debd
Zakaria <zakariamesrar49@gmail.com>
2024-05-10 15:11:26 +0100

    admin login commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -66,6 +66,21 @@ export const updateUser = async (req, res) => {
 };
 
 
+export const deleteUser =  async (req, res) => {
+	try {
+	    const id = req.params.id;
+	    const deletedUser = await UsersModel.findByIdAndDelete(id);
+ 
+	    if (!deletedUser) {
+		   return res.status(404).json({ error: 'User not found' });
+	    }
+ 
+	    res.json({ data: deletedUser });
+	} catch (err) {
+	    res.status(500).json({ error: err.message });
+	}
+ };
+
 
 
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar , getAdminForSidebar , getAllUsers , updateUser } from "../controllers/user.controller.js";
+import { getUsersForSidebar, getAdminForSidebar, getAllUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
 router.get("/admin", protectRoute, getAdminForSidebar);
-router.get("/fetchUsers" , getAllUsers);
-router.put("/updateUserById" , updateUser);
+router.get("/fetchUsers", getAllUsers);
+router.put("/updateUserById/:id", updateUser);
+router.delete("/deleteUserById/:id", deleteUser);
 
 export default router;
