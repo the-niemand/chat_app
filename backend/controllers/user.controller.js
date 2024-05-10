a469f8edc70e30dfd66caf3f741cd63cd926c06c
Zakaria <zakariamesrar49@gmail.com>
2024-05-10 17:29:26 +0100

    admin login commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -40,6 +40,23 @@ export const getAllUsers = async (req, res) => {
 	}
 };
 
+
+export const getUserById = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const User = await User.findById(id)
+		if (!User) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+		res.status(200).json(User);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+
+
 export const updateUser = async (req, res) => {
 	try {
 		const id = req.params.id;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar, getAdminForSidebar, getAllUsers, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { getUsersForSidebar, getAdminForSidebar, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
 router.get("/admin", protectRoute, getAdminForSidebar);
 router.get("/fetchUsers", getAllUsers);
+router.get("/fetchUser/:id", getUserById);
 router.put("/updateUserById/:id", updateUser);
 router.delete("/deleteUserById/:id", deleteUser);
 
